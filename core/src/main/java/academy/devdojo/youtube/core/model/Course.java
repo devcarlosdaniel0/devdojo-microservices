cdc60d0ab042a9082297fe7d6ed439438255f8c6
Carlos <cadanielmarchesin@gmail.com>
2024-10-17 15:38:43 -0300

    antes de dar merda

---
+++ core/src/main/java/academy/devdojo/youtube/core/model/Course.java
@@ -0,0 +1,24 @@
+package academy.devdojo.youtube.core.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Course implements AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+
+}
